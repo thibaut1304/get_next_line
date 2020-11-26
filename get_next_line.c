5283c324b58a862d867ffd3740f26d6d4817f4ae
01010100 01101000 01101001 01100010 <thibaut.husser@free.fr>
2020-11-26 15:09:24 +0100

    :tada:

---
+++ get_next_line.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thhusser <thhusser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/24 21:19:05 by thhusser          #+#    #+#             */
+/*   Updated: 2020/11/24 21:19:05 by thhusser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		ft_return(char **str, char **line)
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	while ((*str)[i] != '\n' && (*str)[i])
+		i++;
+	*line = ft_substr(*str, 0, i);
+	if ((*str)[i] == '\n')
+		tmp = ft_strdup(&(*str)[i + 1]);
+	free(*str);
+	*str = tmp;
+	if (!*str)
+		return (0);
+	return (1);
+}
+
+int		readline(int fd, char **str, char **buff)
+{
+	int		res;
+	char	*tmp;
+
+	while ((res = read(fd, *buff, BUFFER_SIZE)) > 0)
+	{
+		(*buff)[res] = 0;
+		tmp = ft_strjoin(*str, *buff);
+		free(*str);
+		*str = tmp;
+		if (ft_strchr(*str, '\n'))
+			break ;
+	}
+	return (res);
+}
+
+int		ft_error(char *buff)
+{
+	free(buff);
+	return (-1);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	int				res;
+	static char		*str;
+	char			*buff;
+
+	if (BUFFER_SIZE <= 0 || fd < 0 || !line)
+		return (-1);
+	buff = (char*)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buff || read(fd, buff, 0) < 0)
+		return (ft_error(buff));
+	res = readline(fd, &str, &buff);
+	free(buff);
+	if (res < 0)
+		return (-1);
+	if (!str)
+	{
+		*line = ft_strdup("");
+		return (0);
+	}
+	res = ft_return(&str, line);
+	return (res);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thhusser <thhusser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/11 13:11:41 by thhusser          #+#    #+#             */
+/*   Updated: 2020/11/11 13:11:41 by thhusser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		get_next_line(int fd, char **line);
+char	*ft_strdup(const char *s1);
+int		ft_strchr(const char *s, int c);
+
+#endif
